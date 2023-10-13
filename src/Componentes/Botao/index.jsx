4ab1a312ff2404a2cdcb6905d01a11f26ecaa221
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-10-13 14:37:35 -0300

    .

--- src/Componentes/Botao/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './botao.module.css'
-
-export default function Botao() {
-  return (
-    <a href="https://go.hotmart.com/N87558322Y" className={style.botao}>IR PARA O SITE OFICIAL</a>
-  )
-}
