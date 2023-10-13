d58cf8d78fb553f1695e86996d6f5c844c34c55b
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-10-13 12:21:00 -0300

    New
    New

---
+++ src/Componentes/Botao/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './botao.module.css'
+
+export default function Botao() {
+  return (
+    <a href="https://go.hotmart.com/N87558322Y" className={style.botao}>IR PARA O SITE OFICIAL</a>
+  )
+}
---
+++ src/Componentes/Video/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Video() {
+    return (
+        <section>
+            <iframe title='Video' src="https://player.vimeo.com/video/862746752?h=0a8a0b465c" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+        </section>
+    )
+}
